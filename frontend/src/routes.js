d253267147a0c5c2689f32aa9c33dc0798a276bd
tremlfl <florian.treml@drei.com>
2019-12-10 16:17:33 +0100

    conversion api

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { spawn } = require('child_process')
 const debug = require('debug')('botium-speech-processing-routes')
 
 const router = express.Router()
@@ -130,6 +131,123 @@ router.get('/api/tts/:language', async (req, res, next) => {
       'Content-Length': buffer.length
     })
     res.end(buffer)
+  } catch (err) {
+    return next(err)
+  }
+})
+
+/**
+ * @swagger
+ * /api/stt/{language}:
+ *   post:
+ *     description: Convert audio file to text
+ *     security:
+ *       - ApiKeyAuth: []
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: language
+ *         description: ISO-639-1 language code
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string 
+ *     requestBody:
+ *       description: Audio file
+ *       content:
+ *         audio/wav:
+ *           schema:
+ *             type: string
+ *             format: binary
+ *     responses:
+ *       200:
+ *         description: Extracted text
+ *         schema:
+ *           properties:
+ *             text:
+ *               type: string
+ */
+router.post('/api/stt/:language', async (req, res, next) => {
+  if (Buffer.isBuffer(req.body)) {
+    try {
+      const { text } = await stt.stt({
+        language: req.params.language,
+        buffer: req.body
+      })
+      res.json({
+        text
+      }).end()
+    } catch (err) {
+      return next(err)
+    }
+  } else {
+    next(new Error('req.body is not a buffer'))
+  }
+})
+
+/**
+ * @swagger
+ * /api/convert/{profile}:
+ *   post:
+ *     description: Convert audio file
+ *     security:
+ *       - ApiKeyAuth: []
+ *     produces:
+ *       - audio/*
+ *     parameters:
+ *       - name: profile
+ *         description: Conversion profile (for example "wav")
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string 
+ *     requestBody:
+ *       description: Audio file
+ *       content:
+ *         audio/*:
+ *           schema:
+ *             type: string
+ *             format: binary
+ *     responses:
+ *       200:
+ *         description: Audio file
+ *         content:
+ *           audio/*:
+ *             schema:
+ *               type: string
+ *               format: binary
+ */
+router.get('/api/convert/:profile', async (req, res, next) => {
+  if (!Buffer.isBuffer(req.body)) {
+    return next(new Error('req.body is not a buffer'))
+  }
+
+  const envVarSox = `BOTIUM_SPEECH_CONVERT_PROFILE_${req.params.profile.toUpperCase()}`
+  if (!process.env[envVarSox]) {
+    return next(new Error(`Environment variable ${envVarSox} empty`))
+  }
+  const cmdLineSox = process.env[envVarSox].split(' ')
+  try {
+    const sox = spawn(cmdLineSox[0], cmdLineSox.slice(1))
+
+    sox.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`)
+    })
+
+    sox.on('close', (code) => {
+      console.log(`sox process exited with code ${code}`)
+      res.sendStatus(200).end()
+    })
+
+    sox.stdin.write(req.body)
+
+    /*
+    res.writeHead(200, {
+      'Content-disposition': `attachment; filename="${name}"`,
+      'Content-Length': buffer.length
+    })
+    res.end(buffer)
+    */
   } catch (err) {
     return next(err)
   }
