514f6fe2d17b61fafbcb3fcddee3a45b45b700c2
Florian Treml <florian.treml@botium.at>
2019-12-11 13:48:38 +0000

    dictate port forward

--- dictate/server.js
+++ dictate/server.js
@@ -3,20 +3,21 @@ const express = require('express')
 
 const app = express()
 app.disable('etag')
+app.set('trust proxy', true)
 const port = process.env.PORT || 56100
 const dictateDir = process.env.DICTATEDIR || './dictate.js'
 
 
 app.get('/', (req, res) => {
-  res.redirect('/demos/mob.html')
+  res.redirect('./demos/mob.html')
 })
 
 app.get('/demos/mob.html', (req, res) => {
-  console.log('mob.html')
   let mobHtml = fs.readFileSync(`${dictateDir}/demos/mob.html`, { encoding: 'utf-8' })
 
-  const sttUrlDe = process.env.STT_URL_DE || `ws://${req.hostname}/stt_de`
-  const sttUrlEn = process.env.STT_URL_EN || `ws://${req.hostname}/stt_en`
+  const wsProtocol = (req.protocol === 'https' ? 'wss:' : 'ws:')
+  const sttUrlDe = process.env.STT_URL_DE || `${wsProtocol}//${req.hostname}/stt_de`
+  const sttUrlEn = process.env.STT_URL_EN || `${wsProtocol}//${req.hostname}/stt_en`
 
   mobHtml = mobHtml.replace(
     '<option value="wss://bark.phon.ioc.ee:8443/dev/duplex-speech-api/ws/speech|wss://bark.phon.ioc.ee:8443/dev/duplex-speech-api/ws/status">eesti keel</option>',
