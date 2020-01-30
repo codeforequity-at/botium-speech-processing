6f24088e23912e09f2cc8e80c79d264c48323e33
ftreml <florian.treml@botium.at>
2020-01-30 22:40:47 +0100

    fs watcher

---
+++ watcher/watch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir -p /app/watch/stt_input_de
+mkdir -p /app/watch/stt_input_en
+mkdir -p /app/watch/stt_output
+mkdir -p /app/watch/temp
+
+inotifywait -m /app/watch/stt_input_de /app/watch/stt_input_en -e create |
+  while read path action file; do
+    if [[ "$path" =~ .*stt_input_de$ ]]; then
+      language=de
+    elif [[ "$file" =~ .*stt_input_en$ ]]; then
+      language=en
+    fi
+    if [[ "$file" =~ .*mp3$ ]]; then
+      profile=mp3tomonowav
+    elif [[ "$file" =~ .*wav$ ]]; then
+      profile=wavtomonowav
+    else
+      mv $path$file /app/watch/stt_output/$file.err_filetype_not_supported
+      continue
+    fi
+    curl -X POST "http://frontend/api/convert/$profile" -H "Content-Type: audio/mp3" -T /app/watch/stt_output/$file -o /app/watch/temp/$file.wav
+    if [ $? -ne 0 ]; then
+      mv $path$file /app/watch/stt_output/$file.err_convert
+      rm -f /app/watch/temp/$file.wav
+      continue
+    fi
+    curl -X POST "http://frontend/api/stt/$language" -H "Content-Type: audio/wav" -T /app/watch/temp/$file.wav | jq -r .text > /app/watch/stt_output/$file.txt
+    if [ $? -ne 0 ]; then
+      mv $path$file /app/watch/stt_output/$file.err_stt
+      rm -f /app/watch/temp/$file.wav
+      continue
+    fi
+    mv $path$file /app/watch/stt_output/$file
+  done
