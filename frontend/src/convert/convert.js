b643866a1749a85032d8424cdc4c5101c13ee2ae
Florian Treml <florian.treml@cyara.com>
2022-10-18 15:14:41 +0200

    BOT-3297 add Content-Duration headers

--- frontend/src/convert/convert.js
+++ frontend/src/convert/convert.js
@@ -1,30 +1,8 @@
 const fs = require('fs')
 const Mustache = require('mustache')
-const { spawn, exec } = require('child_process')
 const { v1: uuidv1 } = require('uuid')
+const { runShellCommand, getSoxFileType, isBufferMP3, getAudioLengthSeconds } = require('../soxi')
 const debug = require('debug')('botium-speech-processing-convert')
-
-const _getSoxFileType = (filename) => {
-  return new Promise((resolve, reject) => {
-    exec(`soxi -t ${filename}`, (err, stdout, stderr) => {
-      if (err) return reject(err)
-      if (stderr) return reject(stderr.trim())
-      resolve(stdout.trim())
-    })
-  })
-}
-
-const _isMP3 = (buf) => {
-  if (!buf || buf.length < 3) {
-    return false
-  }
-  return (buf[0] === 73 &&
-    buf[1] === 68 &&
-    buf[2] === 51) || (
-    buf[0] === 255 &&
-      (buf[1] >= 224)
-  )
-}
 
 const pcmtowav = async (inputBuffer, { sampleRate = 16000, bitDepth = 16, channelCount = 1 }) => {
   const result = await runconvert(`sox -r ${sampleRate} -e signed -b ${bitDepth} -c ${channelCount} {{{input}}} {{{output}}}`, 'output.wav', { inputBuffer, inputType: 'raw' })
@@ -48,11 +26,11 @@ const runconvert = async (cmdLine, outputName, { inputBuffer, inputType, start, 
       throw new Error('conversion process input file not writable')
     }
     if (!inputtype) {
-      if (_isMP3(inputBuffer)) {
+      if (isBufferMP3(inputBuffer)) {
         inputtype = 'mp3'
       } else {
         try {
-          inputtype = await _getSoxFileType(input)
+          inputtype = await getSoxFileType(input)
           debug(`Identified input type: ${inputtype}`)
         } catch (err) {
           debug(`identification of input file type ${input} failed: ${err.message}`)
@@ -84,68 +62,36 @@ const runconvert = async (cmdLine, outputName, { inputBuffer, inputType, start, 
   }
   debug(`cmdLineFull: ${cmdLineFull}`)
 
-  return new Promise((resolve, reject) => {
-    const childProcess = spawn('/bin/sh', ['-c', cmdLineFull])
+  try {
+    await runShellCommand(cmdLineFull, cmdLine.indexOf('{{{input}}}') < 0 ? inputBuffer : null)
 
-    const childProcessErr = []
-    const formatChildProcessErr = (header) => {
-      const lines = [
-        header,
-        ...childProcessErr
-      ].filter(l => l).map(l => l.trim()).filter(l => l)
-      return lines.join('\n')
+    let outputBuffer = null
+    let outputDuration = null
+    try {
+      outputDuration = await getAudioLengthSeconds(output)
+    } catch (err) {
+      debug(`no audio length readable for ${output}: ${err.message}`)
     }
-
-    childProcess.once('exit', (code, signal) => {
-      debug(`conversion process exited with code ${code}, signal ${signal}`)
-      if (code === 0) {
-        try {
-          const outputBuffer = fs.readFileSync(output)
-          fs.unlinkSync(output)
-          resolve({
-            outputName,
-            outputBuffer
-          })
-        } catch (err) {
-          reject(new Error(`conversion process output file ${output} not readable: ${err.message}`))
-        }
-      } else {
-        reject(new Error(formatChildProcessErr(`conversion process exited with failure code ${code}${signal ? `, signal ${signal}` : ''}`)))
-      }
-      if (input) {
-        try {
-          fs.unlinkSync(input)
-        } catch (err) {
-          debug(`conversion process input file ${input} not deleted: ${err.message}`)
-        }
-      }
-    })
-    childProcess.once('error', (err) => {
-      debug(`conversion process failed: ${err.message}`)
-      reject(new Error(formatChildProcessErr(`conversion process failed: ${err.message}`)))
-    })
-    childProcess.stdout.on('error', (err) => {
-      debug('stdout err ' + err)
-      childProcessErr.push(`${err.message}`)
-    })
-    childProcess.stderr.on('error', (err) => {
-      debug('stderr err ' + err)
-      childProcessErr.push(`${err.message}`)
-    })
-    childProcess.stdin.on('error', (err) => {
-      debug('stdin err ' + err)
-      childProcessErr.push(`${err.message}`)
-    })
-    childProcess.stderr.on('data', (data) => {
-      debug('stderr ' + data)
-      childProcessErr.push(`${data}`)
-    })
-
-    if (cmdLine.indexOf('{{{input}}}') < 0) {
-      childProcess.stdin.write(inputBuffer)
+    try {
+      outputBuffer = fs.readFileSync(output)
+      fs.unlinkSync(output)
+    } catch (err) {
+      throw new Error(`conversion process output file ${output} not readable: ${err.message}`)
     }
-    childProcess.stdin.end()
-  })
+    return {
+      outputName,
+      outputBuffer,
+      outputDuration
+    }
+  } finally {
+    if (input) {
+      try {
+        fs.unlinkSync(input)
+      } catch (err) {
+        debug(`conversion process input file ${input} not deleted: ${err.message}`)
+      }
+    }
+  }
 }
 
 module.exports = {
