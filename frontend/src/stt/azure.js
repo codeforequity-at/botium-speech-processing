78c0df5ae66fb69164168bb0b7850612da18315a
Paul Pröll <paul.proell@hotmail.com>
2023-02-21 15:19:52 +0100

    error mock test

--- frontend/src/stt/azure.js
+++ frontend/src/stt/azure.js
@@ -81,6 +81,13 @@ class AzureSTT {
       events.emit('data', event)
     }
     recognizer.startContinuousRecognitionAsync()
+    setTimeout(() => {
+      const event = {
+        status: 'error',
+        err: 'test'
+      }
+      events.emit('data', event)
+    }, 10000)
 
     return new Promise((resolve, reject) => {
       /* recognizer.canceled = (s, e) => {
